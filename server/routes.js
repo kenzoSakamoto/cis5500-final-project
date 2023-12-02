6d884a0988c088cfdc2f5840f8b6bbb9003c9da0
AdilShahid17 <adilsha1798@gmail.com>
2023-12-01 20:54:11 -0500

    routes 5-9 added

--- server/routes.js
+++ server/routes.js
@@ -11,3 +11,184 @@ const connection = mysql.createConnection({
   database: config.rds_db
 });
 connection.connect((err) => err && console.log(err));
+
+
+
+// Route 5: GET /most_valued_companies
+const most_valued_companies = async function(req, res) {
+  const page = req.query.page;
+  const pageSize = req.query.page_size ?? 10;
+
+  if (!page) {
+    connection.query(`
+      SELECT Name, MarketCap
+      FROM Stocks
+      ORDER BY MarketCap DESC   
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    const offset = pageSize * (page-1);
+
+    connection.query(`
+      SELECT Name, MarketCap
+      FROM Stocks
+      ORDER BY MarketCap DESC
+      LIMIT ${pageSize}
+      OFFSET ${offset}
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  }
+}
+
+// Route 6: GET /correlation
+const correlation = async function(req, res) {
+  const page = req.query.page;
+  const pageSize = req.query.page_size ?? 10;
+
+  if (!page) {
+    connection.query(`
+    WITH StockReturns AS (
+      SELECT
+      SP1.Ticker AS Ticker1,
+      SP2.Ticker AS Ticker2,
+      CORR(SP1.Close, SP2.Close) AS CorrelationCoefficient
+      FROM
+      SecurityPrices SP1
+      INNER JOIN SecurityPrices SP2 ON SP1.Ticker < SP2.Ticker
+      WHERE
+      SP1.Ticker <> SP2.Ticker
+      GROUP BY
+      SP1.Ticker, SP2.Ticker
+    )
+    SELECT
+      S1.Name AS Stock1_Name,
+      S2.Name AS Stock2_Name,
+      SR.CorrelationCoefficient
+    FROM
+    StockReturns SR
+    INNER JOIN Securities S1 ON SR.Ticker1 = S1.Ticker
+    INNER JOIN Securities S2 ON SR.Ticker2 = S2.Ticker
+    ORDER BY
+    SR.CorrelationCoefficient DESC
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    const offset = pageSize * (page-1);
+
+    connection.query(`
+    WITH StockReturns AS (
+      SELECT
+      SP1.Ticker AS Ticker1,
+      SP2.Ticker AS Ticker2,
+      CORR(SP1.Close, SP2.Close) AS CorrelationCoefficient
+      FROM
+      SecurityPrices SP1
+      INNER JOIN SecurityPrices SP2 ON SP1.Ticker < SP2.Ticker
+      WHERE
+      SP1.Ticker <> SP2.Ticker
+      GROUP BY
+      SP1.Ticker, SP2.Ticker
+    )
+    SELECT
+      S1.Name AS Stock1_Name,
+      S2.Name AS Stock2_Name,
+      SR.CorrelationCoefficient
+    FROM
+    StockReturns SR
+    INNER JOIN Securities S1 ON SR.Ticker1 = S1.Ticker
+    INNER JOIN Securities S2 ON SR.Ticker2 = S2.Ticker
+    ORDER BY
+    SR.CorrelationCoefficient DESC
+    LIMIT ${pageSize}
+    OFFSET ${offset}
+    `, (err, data) => {
+      if (err || data.length === 0) {
+        console.log(err);
+        res.json([]);
+      } else {
+        res.json(data);
+      }
+    });
+  }
+}
+
+
+// Route 7: GET /price_trend/:ticker
+const price_trend = async function(req, res) {
+  connection.query(`
+    SELECT Date, Close
+    FROM SecurityPrices
+    WHERE Ticker = '${req.params.ticker}'
+    AND Date >= '${req.query.start_date}'
+    AND Date <= '${req.query.end_date}'
+    ORDER BY Date ASC
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json({});
+    } else {
+      res.json(data[0]);
+    }
+  });
+}
+
+// Route 8: GET /stock_news/:ticker
+const stock_news = async function(req, res) {
+  connection.query(`
+    SELECT Headline, Date
+    FROM FinancialNews
+    WHERE Ticker = '${req.params.ticker}'
+    ORDER BY Date DESC 
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json({});
+    } else {
+      res.json(data[0]);
+    }
+  });
+}
+
+// Route 9: GET /price_trend/:ticker
+const profit_and_loss_statement = async function(req, res) {
+  connection.query(`
+    SELECT
+    Year,
+    Quarter,
+    SUM(CASE WHEN Indicator = 'Final Revenue' THEN Amount ELSE 0 END) AS Revenue,
+    SUM(CASE WHEN Indicator = 'Gross Profit' THEN Amount ELSE 0 END) AS GrossProfit,
+    SUM(CASE WHEN Indicator = 'Operating Income (Loss)' THEN Amount ELSE 0 END) AS
+    OperatingIncome,
+    SUM(CASE WHEN Indicator = 'Net Income (Loss)' THEN Amount ELSE 0 END) AS NetIncome
+    FROM StockFinancials
+    WHERE Ticker = '${req.params.ticker}'
+    AND Year = '${req.query.Year}'
+    AND Quarter = '${req.query.Quarter}'
+    GROUP BY Year, Quarter;
+  `, (err, data) => {
+    if (err || data.length === 0) {
+      console.log(err);
+      res.json({});
+    } else {
+      res.json(data[0]);
+    }
+  });
+}
--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ app.use(cors({
   origin: '*',
 }));
 
+app.get('/most_valued_companies', routes.most_valued_companies);
+app.get('/correlation', routes.correlation);
+app.get('/price_trend/:ticker', routes.price_trend);
+app.get('/stock_news/:ticker', routes.stock_news);
+app.get('/profit_and_loss_statement/:ticker', routes.profit_and_loss_statement);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)

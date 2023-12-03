f0c44d6f73582b261321f2ba0e8576a919ff89eb
julzhao6 <julia@zdhome.net>
2023-12-02 22:11:29 -0500

    routes1-4

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,12 @@ const config = require('./config.json')
 
 // Creates MySQL connection using database credential provided in config.json
 // Do not edit. If the connection fails, make sure to check that config.json is filled out correctly
+
+app.get('/signin', routes.signin);
+app.get('/topstock', routes.topstock);
+app.get('/tradedstock', routes.tradedstock);
+app.get('/volatilestock', routes.volatiestock);
+
 const connection = mysql.createConnection({
   host: config.rds_host,
   user: config.rds_user,

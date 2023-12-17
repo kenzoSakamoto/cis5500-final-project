043ca575bafd711881b1eb08d78c299d0774c723
julzhao6 <julia@zdhome.net>
2023-12-16 21:39:01 -0500

    tests updated

--- server/server/__tests__/tests.js
+++ server/server/__tests__/tests.js
@@ -122,9 +122,11 @@ test('GET /correlation', async () => {
   await supertest(app).get('/correlation?page_size=1')
     .expect(200)
     .then((res) => {
-      expect(res.body.length).toStrictEqual(190)
+      expect(res.body.length).toStrictEqual(45)
     });
 });
+
+
 
 test('GET /profit_and_loss_statement/:ticker', async () => {
   await supertest(app).get('/profit_and_loss_statement/AAPL?year=2014&quarter=Q1')
@@ -172,5 +174,40 @@ test('GET /news_recommendation', async () => {
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.newsrecs);
+    });
+});
+
+test('GET /stock_info', async () => {
+  await supertest(app).get('/stock_info/AAPL')
+    .expect(200)
+    .then((res) => {
+      expect(res.body).toStrictEqual([{
+        "CEO": "Timothy D. Cook",
+        "description": "Apple Inc is designs, manufactures and markets mobile communication and media devices and personal computers, and sells a variety of related software, services, accessories, networking solutions and third-party digital content and applications.",
+        "exchange": "Nasdaq Global Select",
+        "industry": "Computer Hardware",
+        "market_cap": 807491700000,
+        "name": "Apple",
+        "ticker": "AAPL",
+        "website": "http://www.apple.com",
+
+      }]);
+    });
+});
+
+test('GET /moving_avg', async () => {
+  await supertest(app).get('/moving_avg/0')
+    .expect(200)
+    .then((res) => {
+      expect(res.body.length).toStrictEqual(14762);
+      expect(res.body[0]).toStrictEqual({"company": "Copart", "date": "1994-03-17T05:00:00.000Z", "moving_average": 0.61});
+    });
+});
+
+test('GET /correlation page', async () => {
+  await supertest(app).get('/correlation?page_size=1&page=1')
+    .expect(200)
+    .then((res) => {
+      expect(res.body.length).toStrictEqual(1)
     });
 });
